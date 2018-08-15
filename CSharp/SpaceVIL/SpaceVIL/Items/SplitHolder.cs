8daf4738a867ec3c3a6829fdd5da4ced5190f911
Valeriia <VSedaikina@gmail.com>
2018-08-15 15:21:14 +0300

    split holder

--- CSharp/SpaceVIL/SpaceVIL/Items/SplitHolder.cs
+++ CSharp/SpaceVIL/SpaceVIL/Items/SplitHolder.cs
@@ -4,9 +4,20 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace SpaceVIL.Items
+namespace SpaceVIL
 {
-    class SplitHolder
+    class SplitHolder : VisualItem, IDraggable
     {
+        private static int count = 0;
+        private Orientation _orientation;
+        public SplitHolder()
+        {
+            SetItemName("SplitHolder_" + count);
+            count++;
+        }
+
+        public void SetOrientation(Orientation or) {
+            _orientation = or;
+        }
     }
 }
