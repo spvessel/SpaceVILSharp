749e201bbd457413c154a413642b670eff343a14
rsedaikin <rsedaikin@gmail.com>
2018-09-06 21:13:14 +0300

    embedded fonts added

--- CSharp/SpaceVIL/SpaceVIL/Common/DefaultsService.cs
+++ CSharp/SpaceVIL/SpaceVIL/Common/DefaultsService.cs
@@ -1,9 +1,22 @@
 using System.Drawing;
+using System.Drawing.Text;
 using System;
 using System.Collections.Generic;
+using System.Reflection;
+using System.IO;
 
 namespace SpaceVIL
 {
+    public enum EmbeddedFont
+    {
+        OpenSans,
+        Moire,
+        Verdana,
+        Muli,
+        Nunito,
+        Quicksand,
+        Ubuntu
+    }
     public static class DefaultsService
     {
         private static ThemeStyle _default_theme = ThemeStyle.GetInstance();
@@ -28,6 +41,39 @@ namespace SpaceVIL
         {
             return _default_theme.GetThemeStyle(type);
         }
+        private static String _embedded_font;
+        public static void SetDefaultEmbeddedFont(EmbeddedFont font)
+        {
+            switch (font)
+            {
+                case EmbeddedFont.Verdana:
+                    _embedded_font = "SpaceVIL.Fonts.verdana.ttf";
+                    break;
+                case EmbeddedFont.Moire:
+                    _embedded_font = "SpaceVIL.Fonts.moireregular.ttf";
+                    break;
+                case EmbeddedFont.OpenSans:
+                    _embedded_font = "SpaceVIL.Fonts.opensans.ttf";
+                    break;
+
+                case EmbeddedFont.Muli:
+                    _embedded_font = "SpaceVIL.Fonts.Muli-Regular.ttf";
+                    break;
+                case EmbeddedFont.Nunito:
+                    _embedded_font = "SpaceVIL.Fonts.Nunito-Regular.ttf";
+                    break;
+                case EmbeddedFont.Quicksand:
+                    _embedded_font = "SpaceVIL.Fonts.Quicksand-Regular.ttf";
+                    break;
+                case EmbeddedFont.Ubuntu:
+                    _embedded_font = "SpaceVIL.Fonts.Ubuntu-Regular.ttf";
+                    break;
+
+                default:
+                    _embedded_font = "SpaceVIL.Fonts.verdana.ttf";
+                    break;
+            }
+        }
         public static void SetDefaultFont(Font font)
         {
             _default_font = font;
@@ -35,9 +81,36 @@ namespace SpaceVIL
         public static Font GetDefaultFont()//объекты не инициализируются почему-то, выяснить
         {
             if (_default_font == null)
-                _default_font = new Font(new FontFamily("Arial"), 14, FontStyle.Regular);
+            {
+                FontFamily[] fontFamilies;
+                PrivateFontCollection privateFontCollection = AddFontFromMemory();
+                fontFamilies = privateFontCollection.Families;
+                int count = fontFamilies.Length;
+                string familyName = fontFamilies[0].Name;
+                _default_font = new Font(familyName, 14);
+            }
 
             return _default_font;
+        }
+        private static PrivateFontCollection AddFontFromMemory()
+        {
+            if (_embedded_font == null || _embedded_font == String.Empty)
+                SetDefaultEmbeddedFont(EmbeddedFont.Moire);
+
+            PrivateFontCollection privateFontCollection = new PrivateFontCollection();
+            Stream fontStream = Assembly.GetExecutingAssembly().GetManifestResourceStream(_embedded_font);
+
+            byte[] fontdata = new byte[fontStream.Length];
+            fontStream.Read(fontdata, 0, (int)fontStream.Length);
+            fontStream.Close();
+            unsafe
+            {
+                fixed (byte* pFontData = fontdata)
+                {
+                    privateFontCollection.AddMemoryFont((System.IntPtr)pFontData, fontdata.Length);
+                }
+            }
+            return privateFontCollection;
         }
     }
 }
--- CSharp/SpaceVIL/SpaceVIL/compile.bat
+++ CSharp/SpaceVIL/SpaceVIL/compile.bat
@@ -10,4 +10,11 @@
 -res:Shaders\vs_fill.glsl,SpaceVIL.Shaders.vs_fill.glsl ^
 -res:Shaders\fs_texture.glsl,SpaceVIL.Shaders.fs_texture.glsl ^
 -res:Shaders\vs_texture.glsl,SpaceVIL.Shaders.vs_texture.glsl ^
+-res:Fonts\verdana.ttf,SpaceVIL.Fonts.verdana.ttf ^
+-res:Fonts\moireregular.ttf,SpaceVIL.Fonts.moireregular.ttf ^
+-res:Fonts\opensans.ttf,SpaceVIL.Fonts.opensans.ttf ^
+-res:Fonts\Muli-Regular.ttf,SpaceVIL.Fonts.Muli-Regular.ttf ^
+-res:Fonts\Nunito-Regular.ttf,SpaceVIL.Fonts.Nunito-Regular.ttf ^
+-res:Fonts\Quicksand-Regular.ttf,SpaceVIL.Fonts.Quicksand-Regular.ttf ^
+-res:Fonts\Ubuntu-Regular.ttf,SpaceVIL.Fonts.Ubuntu-Regular.ttf ^
 -recurse:*.cs
