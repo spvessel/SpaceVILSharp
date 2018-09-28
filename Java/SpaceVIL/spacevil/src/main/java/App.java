0176e5067c24f00cc99229d736c997357561448b
rsedaikin <rsedaikin@gmail.com>
2018-09-28 14:19:38 +0300

    finish gradle project

--- Java/SpaceVIL/spacevil/src/main/java/App.java
+++ Java/SpaceVIL/spacevil/src/main/java/App.java
@@ -1,8 +1,10 @@
+package app;
+
 import com.spvessel.View.MainWindow;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello World! My dear friend!");
+        System.out.println("Hello World! My dear friend! I'm here!");
         MainWindow mw = new MainWindow();
         mw.show();
 //        WindowLayoutBox.getWindowInstance("MainWindow").show();
