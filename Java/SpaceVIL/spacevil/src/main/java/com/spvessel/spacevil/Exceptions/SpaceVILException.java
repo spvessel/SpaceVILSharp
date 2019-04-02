2fb7757a97ed772741c61fe95638646e15109c90
spvessel <spvessel@gmail.com>
2019-04-02 20:25:10 +0300

    Update SpaceVILException.java

--- Java/SpaceVIL/spacevil/src/main/java/com/spvessel/spacevil/Exceptions/SpaceVILException.java
+++ Java/SpaceVIL/spacevil/src/main/java/com/spvessel/spacevil/Exceptions/SpaceVILException.java
@@ -1,6 +1,6 @@
 package com.spvessel.spacevil.Exceptions;
 
-public class SpaceVILException extends Exception {
+public class SpaceVILException extends RuntimeException {
     private static final long serialVersionUID = 1L;
 
     // static final long serialVersionUID = 0;
