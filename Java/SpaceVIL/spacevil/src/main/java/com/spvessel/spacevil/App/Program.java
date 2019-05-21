02597bc973ba9426c49d20e06c01bb281fe14a55
spvessel <spvessel@gmail.com>
2019-05-21 15:21:03 +0300

    Update Program.java

--- Java/SpaceVIL/spacevil/src/main/java/com/spvessel/spacevil/App/Program.java
+++ Java/SpaceVIL/spacevil/src/main/java/com/spvessel/spacevil/App/Program.java
@@ -45,7 +45,7 @@ public class Program {
         // ScissorsTest sc = new ScissorsTest();
         // sc.show();
 
-        mw.show();
+        // mw.show();
         
         // lt.show();
         // con.show();
