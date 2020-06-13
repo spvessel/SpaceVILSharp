e1bf777cb00e735a702ee7cb2ca5abbcab98a8da
spvessel <spvessel@gmail.com>
2020-06-13 17:38:57 +0300

    OwlsInSpace - fix KeyWordItem

--- OwlsInSpace/src/main/java/OwnLibs/Owls/Views/Items/KeyWordItem.java
+++ OwlsInSpace/src/main/java/OwnLibs/Owls/Views/Items/KeyWordItem.java
@@ -29,7 +29,7 @@ public class KeyWordItem extends Label {
     }
 
     @Override
-    public void setText(String text) {
+    public void setText(Object text) {
         super.setText(text);
         setWidth(getTextWidth() + 10);
     }
