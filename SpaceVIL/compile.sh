362ea4bbb7d67e349b1240103de75cf421a4f9c3
Roman <45689928+spvessel@users.noreply.github.com>
2022-09-16 12:39:14 +0300

    Refactored compile and run scripts for linux. (#3)
    Co-authored-by: Roman Sedaikin <rsedaikin@gmail.com>

--- SpaceVIL/compile.sh
+++ SpaceVIL/compile.sh
@@ -1,3 +1,6 @@
+# how install [dotnet] and [nuget]:
+# https://docs.microsoft.com/en-us/dotnet/core/install/linux-ubuntu
+# https://docs.microsoft.com/en-us/nuget/install-nuget-client-tools#cli-tools
 cd ..
 rm -rf com.spvessel.spacevil
 mkdir com.spvessel.spacevil
